52f00fdf05f98f9258fcf3e143c683b2acc7487b
ShawHrown <shawhrown@163.com>
2020-08-18 11:22:31 +0800

    feat: add shell timeout

--- corelib/src/datasource/agent/shell.rs
+++ corelib/src/datasource/agent/shell.rs
@@ -1,30 +1,36 @@
 use crate::datasource::BashRow;
-use crate::{Error, Promise, Result, ToData, ToType};
-use std::io::{BufRead, Cursor};
+use crate::{Promise, Result, ToData, ToType};
+use std::io::Read;
 use std::process::Command;
+use std::time::Duration;
+use timeout_readwrite::TimeoutReadExt;
 
 #[datasource]
-fn shell(script: String, _timeout: u32, promise: &mut Promise<BashRow>) -> Result<()> {
-    let output = Command::new("sh").arg("-c").arg(script).output()?;
-    if output.status.success() {
-        let mut cur = Cursor::new(output.stdout).lines();
-        let mut index = 0;
-        while let Some(line) = cur.next() {
-            let line = line?;
-            promise.commit(BashRow {
-                line,
-                line_num: index,
-            })?;
-            index += 1;
-        }
-        Ok(())
-    } else {
-        let msg = String::from_utf8(output.stderr).unwrap_or("".to_owned());
-        return Err(Error::from(std::io::Error::new(
-            std::io::ErrorKind::UnexpectedEof,
-            format!("exit code: {:?} - {}", output.status, msg),
-        )));
+fn shell(script: String, timeout: u32, promise: &mut Promise<BashRow>) -> Result<()> {
+    let child = Command::new("sh")
+        .arg("-c")
+        .arg(script)
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::null())
+        .spawn()?;
+    let stdout = child.stdout.ok_or(std::io::Error::new(
+        std::io::ErrorKind::InvalidInput,
+        "lose stdout",
+    ))?;
+
+    let mut data = String::new();
+    let _ = stdout.with_timeout(Duration::from_secs(timeout as u64)).read_to_string(&mut data)?;
+    
+    let mut cur = data.lines();
+    let mut index = 0;
+    while let Some(line) = cur.next() {
+        promise.commit(BashRow {
+            line: line.to_owned(),
+            line_num: index,
+        })?;
+        index += 1;
     }
+    Ok(())
 }
 
 #[test]
@@ -54,4 +60,22 @@ fn test() {
         index += 1;
     }
     assert!(index > 0);
+}
+
+#[test]
+#[should_panic(expected = "timed out")]
+fn test_timeout() {
+    use crate::*;
+    let (req, resp) = crate::new_req(crate::Args::new(), std::time::Duration::from_secs(2));
+    {
+        let mut promise = req.head::<BashRow>().unwrap();
+        shell("sleep 10;echo 'Hello world'".to_string(), 2, &mut promise).unwrap();
+        drop(req);
+    }
+
+    let resp = resp.wait().unwrap();
+    assert_eq!(
+        &columns![String: "line",Integer: "line_num"],
+        resp.columns()
+    );
 }
